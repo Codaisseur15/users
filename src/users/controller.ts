a5171cfb27beb6584fbfb2e50d6641948d1de0b9
SiaGian <32592217+SiaGiann@users.noreply.github.com>
2018-03-29 15:55:44 +0200

    Update controller.ts

--- src/users/controller.ts
+++ src/users/controller.ts
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import { JsonController, Post, Param, Get, Put, Patch, NotFoundError, Body, Authorized, Delete } from 'routing-controllers'
 import User from './entity';
 import * as request from 'superagent'
 
 const eventUrl = process.env.EVENT_URL || 'http://localhost:5008/events'
 
-// this makes sure a class is marked as controller that always returns JSON
-=======
-import { JsonController, Post, Param, Get, Put, Patch, NotFoundError, Body, Delete, Authorized } from 'routing-controllers'
-import User from './entity';
-
->>>>>>> 3948dd2d99fa596bf544727421d0577dacb6d7f0
 @JsonController()
 export default class UserController {
 
@@ -21,7 +14,6 @@ export default class UserController {
     const {password, ...rest} = user
     const entity = User.create(rest)
     await entity.setPassword(password)
-<<<<<<< HEAD
     await request.post(eventUrl)
     .send({event: 'newuser',
       data: {
@@ -30,7 +22,6 @@ export default class UserController {
         role: entity.role
       }
     })
-
     return entity.save()
   }
 
@@ -40,15 +31,12 @@ export default class UserController {
   // @Authorized()
   @Get('/users/:id([0-9]+)')
   getUser(
-    // this decorator retrieves the ID parameter from the url
-=======
     return entity.save()
   }
 
   // @Authorized()
   @Get('/users/:id([0-9]+)')
   getUser(
->>>>>>> 3948dd2d99fa596bf544727421d0577dacb6d7f0
     @Param('id') id: number
   ) {
     return User.findOneById(id)
@@ -60,11 +48,8 @@ export default class UserController {
     return User.find()
   }
 
-<<<<<<< HEAD
   // Add update: @patch
   //Change @body for our purposes
-=======
->>>>>>> 3948dd2d99fa596bf544727421d0577dacb6d7f0
   // @Authorized()
   @Put('/users/:id([0-9]+)')
   @Patch('/users/:id([0-9]+)')
@@ -73,17 +58,12 @@ export default class UserController {
     @Body() update: Partial<User>
   ) {
     const user = await User.findOneById(id)
-<<<<<<< HEAD
     if (!user) throw new NotFoundError('Cannot find page')
-=======
     if (!user) throw new NotFoundError('Cannot find user')
->>>>>>> 3948dd2d99fa596bf544727421d0577dacb6d7f0
-
     return User.merge(user, update).save()
   }
 
   // @Authorized()
-<<<<<<< HEAD
  @Delete('/users/:id([0-9]+)')
  async removeUser(
    @Param('id') id: number
@@ -93,16 +73,4 @@ export default class UserController {
      user.remove()
      return "user succesfully deleted"
    }
-=======
-  @Delete('/users/:id([0-9]+)')
-  async removeUser(
-    @Param('id') id: number
-  )  {
-      const user = await User.findOneById(id)
-      if (!user) throw new NotFoundError('Cannot find user')
-      user.remove()
-      return "user succesfully deleted"
-    }
->>>>>>> 3948dd2d99fa596bf544727421d0577dacb6d7f0
-
 }
